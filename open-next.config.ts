c70ca4bcd623e8854346007cb3ac683b732f768b
davidoyle <stopusingthisusername@gmail.com>
2025-11-29 19:20:40 -0500

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "open-next";
 const config: Config = {
   outDir: ".vercel/output",
   nextjs: {
-    // IMPORTANT: enable pages router support
     usePagesRouter: true,
   },
   middleware: false,
